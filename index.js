f0bad741b000a1cd9847d7824ac55d7555409ce9
PhamVanBach <bachpham8520@gmail.com>
2020-10-24 09:19:49 +0700

    this folder to add boostrap for express

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var useRouter = require('./router/user.router');
+
+var app = express();
+var port = 3000;
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function(req, res){
+    res.render('index', {
+        name:'Boss'
+    });
+});
+
+//user router
+app.use('/users', useRouter);
+
+app.listen(port, function(){
+    console.log('Server listening on port' + port);
+})
