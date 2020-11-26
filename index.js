00777bca65993358e2f37ceaf0e1b5c9107ed782
PhamVanBach <bachpham8520@gmail.com>
2020-11-26 15:37:31 +0700

    add function signedCookies

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 var express = require("express");
+
+var bodyPaser = require("body-parser");
+var cookieParser = require('cookie-parser');
+
+//MiddleWare
 var useRouter = require('./router/user.router');
+var authRoter = require('./router/auth.router');
+
+var authMiddleware = require('./middlewares/auth.middleware');
+
+var port = 3000;
 
 var app = express();
-var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyPaser.json()) // for parsing application/json
+app.use(bodyPaser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser("qerwergdfdsf0154782"));
+
 app.use(express.static('public'));
 
+ //Routes
 app.get('/', function(req, res){
     res.render('index', {
         name:'Boss'
     });
 });
 
-//user router
-app.use('/users', useRouter);
+app.use('/users', authMiddleware.requireAuth , useRouter);
+app.use('/auth', authRoter);
 
 app.listen(port, function(){
     console.log('Server listening on port' + port);
--- router/user.router.js
+++ router/user.router.js
@@ -1,8 +1,17 @@
 var express = require('express');
+
+var controllers = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+
 var router = express.Router();
-var controllers = require('../controllers/user.controller')
 
 router.get('/', controllers.index);
+
+router.get('/cookie', function(req, res){
+    res.cookie('user-id', 12345);
+    res.send("hehehehe");
+});
+
 
 router.get('/search', controllers.search);
 
@@ -10,6 +19,6 @@ router.get('/create', controllers.create);
 
 router.get("/:id", controllers.getID);
 
-router.post('/create', controllers.postCreate);
+router.post('/create', validate.postCreate ,controllers.postCreate);
 
 module.exports = router;
